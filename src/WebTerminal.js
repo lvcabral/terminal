ac2d8db7956426837f3482224a1d60ebc551ad6d
Marcelo Lv Cabral <marcelo@lvcabral.com>
2025-10-14 21:55:38 -0700

    Fixed `setColorTheme` to dynamically changed previous text in terminal

--- src/WebTerminal.js
+++ src/WebTerminal.js
@@ -1,36 +1,36 @@
 /* global MutationObserver */
 
-import { 
-  cloneCommandNode, 
-  COMMANDS, 
-  markup, 
-  ansiToHtml, 
-  colors, 
-  colorize, 
-  contextualColorize, 
-  colorizeMessageType, 
-  colorizeJSON, 
+import {
+  cloneCommandNode,
+  COMMANDS,
+  markup,
+  ansiToHtml,
+  colors,
+  colorize,
+  contextualColorize,
+  colorizeMessageType,
+  colorizeJSON,
   colorizeCommandOutput,
-  COLOR_THEMES 
-} from './modules';
-import style from './WebTerminal.css'; // eslint-disable-line
+  COLOR_THEMES,
+} from "./modules";
+import style from "./WebTerminal.css"; // eslint-disable-line
 
-const KEY = 'WebTerm';
+const KEY = "WebTerm";
 
 const { addEventListener, localStorage } = window;
 
 class Terminal {
   constructor(props = {}) {
     const {
-      container = 'web-terminal',
+      container = "web-terminal",
       commands = {},
       welcome = 'Welcome to the simple <a href="">Web Terminal</a>.',
-      prompt = '',
-      separator = '&gt;',
+      prompt = "",
+      separator = "&gt;",
       ignoreBadCommand = false,
       autoFocus = true,
       colors: contextualColors = true,
-      colorTheme = 'light',
+      colorTheme = "light",
     } = props;
     this.commands = Object.assign({}, COMMANDS, commands);
     this.history = localStorage[KEY] ? JSON.parse(localStorage[KEY]) : [];
@@ -60,19 +60,19 @@ class Terminal {
   cacheDOM = (el) => {
     el.classList.add(KEY);
     el.classList.add(`theme-${this.colorTheme}`);
-    el.insertAdjacentHTML('beforeEnd', markup(this));
+    el.insertAdjacentHTML("beforeEnd", markup(this));
 
     // Cache DOM nodes
-    const container = el.querySelector('.container');
+    const container = el.querySelector(".container");
     this.DOM = {
       container,
-      output: container.querySelector('output'),
-      command: container.querySelector('.command'),
-      input: container.querySelector('.command .input'),
-      prompt: container.querySelector('.command .prompt'),
+      output: container.querySelector("output"),
+      command: container.querySelector(".command"),
+      input: container.querySelector(".command .input"),
+      prompt: container.querySelector(".command .prompt"),
       el, // Store reference to main element for theme switching
     };
-  }
+  };
 
   addListeners = () => {
     const { DOM, autoFocus } = this;
@@ -85,28 +85,33 @@ class Terminal {
     // Start observing the output element for child list changes
     observer.observe(DOM.output, { childList: true, subtree: true });
 
-    DOM.output.addEventListener('click', event => event.stopPropagation(), false);
-    DOM.input.addEventListener('keyup', this.onKeyUp, false);
-    DOM.input.addEventListener('keydown', this.onKeyDown, false);
-    DOM.command.addEventListener('click', () => DOM.input.focus(), false);
+    DOM.output.addEventListener("click", (event) => event.stopPropagation(), false);
+    DOM.input.addEventListener("keyup", this.onKeyUp, false);
+    DOM.input.addEventListener("keydown", this.onKeyDown, false);
+    DOM.command.addEventListener("click", () => DOM.input.focus(), false);
 
     if (autoFocus) {
-      addEventListener('click', () => DOM.input.focus(), false);
+      addEventListener("click", () => DOM.input.focus(), false);
 
-      addEventListener('keyup', (event) => {
-        DOM.input.focus();
-        event.stopPropagation();
-        event.preventDefault();
-      }, false);
+      addEventListener(
+        "keyup",
+        (event) => {
+          DOM.input.focus();
+          event.stopPropagation();
+          event.preventDefault();
+        },
+        false
+      );
     }
-  }
+  };
 
   onKeyUp = (event) => {
     const { keyCode } = event;
     const { DOM, history = [], historyCursor } = this;
 
-    if (keyCode === 27) { // ESC key
-      DOM.input.value = '';
+    if (keyCode === 27) {
+      // ESC key
+      DOM.input.value = "";
       event.stopPropagation();
       event.preventDefault();
     } else if ([38, 40].includes(keyCode)) {
@@ -115,16 +120,14 @@ class Terminal {
 
       if (history[this.historyCursor]) DOM.input.value = history[this.historyCursor];
     }
-  }
+  };
 
   onKeyDown = ({ keyCode }) => {
-    const {
-      commands = {}, DOM, history, onInputCallback, state,
-    } = this;
+    const { commands = {}, DOM, history, onInputCallback, state } = this;
     const commandLine = DOM.input.value.trim();
     if (keyCode !== 13 || !commandLine) return;
 
-    const [command, ...parameters] = commandLine.split(' ');
+    const [command, ...parameters] = commandLine.split(" ");
 
     if (state.prompt) {
       state.prompt = false;
@@ -143,8 +146,8 @@ class Terminal {
     DOM.output.appendChild(cloneCommandNode(DOM.command));
 
     // Clean command line
-    DOM.command.classList.add('hidden');
-    DOM.input.value = '';
+    DOM.command.classList.add("hidden");
+    DOM.input.value = "";
 
     // Dispatch command
     let handled = false;
@@ -158,19 +161,19 @@ class Terminal {
       this.output(`<u>${command}</u>: command not found.`);
     }
     if (onInputCallback) onInputCallback(command, parameters, handled);
-  }
+  };
 
   resetCommand = () => {
     const { DOM } = this;
 
-    DOM.input.value = '';
-    DOM.command.classList.remove('input');
-    DOM.command.classList.remove('hidden');
+    DOM.input.value = "";
+    DOM.command.classList.remove("input");
+    DOM.command.classList.remove("hidden");
     if (DOM.input.scrollIntoView) DOM.input.scrollIntoView();
-  }
+  };
 
   clear() {
-    this.DOM.output.innerHTML = '';
+    this.DOM.output.innerHTML = "";
     if (!this.state.idle) {
       this.DOM.input.focus();
     }
@@ -180,16 +183,16 @@ class Terminal {
   idle() {
     const { DOM } = this;
 
-    DOM.command.classList.add('idle');
+    DOM.command.classList.add("idle");
     DOM.prompt.innerHTML = '<div class="spinner"></div>';
   }
 
-  prompt(prompt, callback = () => { }) {
+  prompt(prompt, callback = () => {}) {
     this.state.prompt = true;
     this.onAskCallback = callback;
     this.DOM.prompt.innerHTML = `${prompt}:`;
     this.resetCommand();
-    this.DOM.command.classList.add('input');
+    this.DOM.command.classList.add("input");
   }
 
   onInput(callback) {
@@ -197,38 +200,39 @@ class Terminal {
   }
 
   // Output raw HTML without any processing (useful for pre-formatted HTML content)
-  outputHTML(html = '&nbsp;') {
-    this.DOM.output.insertAdjacentHTML('beforeEnd', `<span>${html}</span>`);
+  outputHTML(html = "&nbsp;") {
+    this.DOM.output.insertAdjacentHTML("beforeEnd", `<span>${html}</span>`);
     this.scrollDown();
   }
 
-  output(html = '&nbsp;', type = null) {
+  output(html = "&nbsp;", type = null) {
     let processedHtml = html;
-    
+
     // Process colors if enabled
-    if (this.contextualColors && typeof html === 'string') {
+    if (this.contextualColors && typeof html === "string") {
       // Handle specific message types first
-      if (type && ['error', 'success', 'warning', 'info'].includes(type)) {
+      if (type && ["error", "success", "warning", "info"].includes(type)) {
         processedHtml = colorizeMessageType(html, type, this.colorTheme);
       }
       // Then apply contextual coloring
       else {
         // First process ANSI codes, then contextual coloring
         processedHtml = ansiToHtml(html);
-        if (processedHtml === html) { // Only apply contextual if no ANSI codes were processed
+        if (processedHtml === html) {
+          // Only apply contextual if no ANSI codes were processed
           processedHtml = contextualColorize(processedHtml, this.colorTheme);
         }
       }
-    } else if (typeof html === 'string') {
+    } else if (typeof html === "string") {
       // If colors are disabled, still process ANSI codes to avoid showing raw escape sequences
       processedHtml = ansiToHtml(html);
       if (processedHtml === html) {
         // No ANSI codes found, escape HTML to prevent injection
-        processedHtml = html.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        processedHtml = html.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
       }
     }
-    
-    this.DOM.output.insertAdjacentHTML('beforeEnd', `<span>${processedHtml}</span>`);
+
+    this.DOM.output.insertAdjacentHTML("beforeEnd", `<span>${processedHtml}</span>`);
     this.resetCommand();
   }
 
@@ -251,10 +255,13 @@ class Terminal {
   }
 
   setPrompt(prompt = this.shell.prompt) {
-    const { DOM, shell: { separator } } = this;
+    const {
+      DOM,
+      shell: { separator },
+    } = this;
 
     this.shell = { prompt, separator };
-    DOM.command.classList.remove('idle');
+    DOM.command.classList.remove("idle");
     DOM.prompt.innerHTML = `${prompt}${separator}`;
     DOM.input.focus();
   }
@@ -267,32 +274,62 @@ class Terminal {
 
   // Convenience methods for message types
   error(text) {
-    this.output(text, 'error');
+    this.output(text, "error");
   }
 
   success(text) {
-    this.output(text, 'success');
+    this.output(text, "success");
   }
 
   warning(text) {
-    this.output(text, 'warning');
+    this.output(text, "warning");
   }
 
   info(text) {
-    this.output(text, 'info');
+    this.output(text, "info");
   }
 
   // Method to change color theme
   setColorTheme(theme) {
     if (COLOR_THEMES[theme]) {
+      const oldTheme = this.colorTheme;
+      const oldColors = COLOR_THEMES[oldTheme];
+      const newColors = COLOR_THEMES[theme];
+
       // Remove old theme class
       this.DOM.el.classList.remove(`theme-${this.colorTheme}`);
-      
+
       // Set new theme
       this.colorTheme = theme;
-      
+
       // Add new theme class
       this.DOM.el.classList.add(`theme-${this.colorTheme}`);
+
+      // Update all existing inline styles in the output to use new theme colors
+      if (this.contextualColors && oldColors && newColors) {
+        const outputSpans = this.DOM.output.querySelectorAll('span[style*="color"]');
+        outputSpans.forEach((span) => {
+          const currentStyle = span.getAttribute("style");
+          if (currentStyle) {
+            let updatedStyle = currentStyle;
+
+            // Replace each old theme color with the corresponding new theme color
+            Object.keys(oldColors).forEach((colorKey) => {
+              const oldColor = oldColors[colorKey];
+              const newColor = newColors[colorKey];
+
+              // Escape special regex characters in the color value (e.g., # in hex colors)
+              const escapedOldColor = oldColor.replace(/[#().]/g, "\\$&");
+
+              // Match the color in the style attribute (case-insensitive)
+              const colorRegex = new RegExp(`color:\\s*${escapedOldColor}`, "gi");
+              updatedStyle = updatedStyle.replace(colorRegex, `color: ${newColor}`);
+            });
+
+            span.setAttribute("style", updatedStyle);
+          }
+        });
+      }
     }
   }
 
--- test/WebTerminal.test.js
+++ test/WebTerminal.test.js
@@ -1,4 +1,4 @@
-import WebTerminal from '../src/WebTerminal';
+import WebTerminal from "../src/WebTerminal";
 
 global.MutationObserver = class {
   constructor(callback) {}
@@ -6,18 +6,18 @@ global.MutationObserver = class {
   observe(element, initObject) {}
 };
 
-describe('WebTerminal', () => {
+describe("WebTerminal", () => {
   beforeEach(() => {
     document.body.innerHTML = '<div id="web-terminal"></div>';
   });
 
-  it('default', () => {
+  it("default", () => {
     expect(WebTerminal).toBeDefined();
 
-    expect(typeof WebTerminal).toEqual('function');
+    expect(typeof WebTerminal).toEqual("function");
   });
 
-  it('when instance', () => {
+  it("when instance", () => {
     const term = new WebTerminal();
 
     expect(term).toBeDefined();
@@ -27,160 +27,190 @@ describe('WebTerminal', () => {
     expect(term.setPrompt).toBeDefined();
   });
 
-  it('when clear()', () => {
+  it("when clear()", () => {
     const term = new WebTerminal();
 
     term.clear();
-    expect(document.querySelector('output').innerHTML).toEqual('');
+    expect(document.querySelector("output").innerHTML).toEqual("");
   });
 
-  it('when idle()', () => {
+  it("when idle()", () => {
     const term = new WebTerminal();
 
     term.idle();
-    expect(document.querySelector('.command .prompt').innerHTML).toEqual('<div class="spinner"></div>');
+    expect(document.querySelector(".command .prompt").innerHTML).toEqual(
+      '<div class="spinner"></div>'
+    );
   });
 
-  it('when onInput()', () => {
+  it("when onInput()", () => {
     const mockCallback = jest.fn();
     const term = new WebTerminal();
 
     term.onInput(mockCallback);
-    mockCallback('hello');
+    mockCallback("hello");
     expect(mockCallback.mock.calls.length).toBe(1);
-    expect(mockCallback.mock.calls[0][0]).toBe('hello');
+    expect(mockCallback.mock.calls[0][0]).toBe("hello");
   });
 
-  it('when output()', () => {
+  it("when output()", () => {
     const term = new WebTerminal({ colors: false }); // Disable colors for this test
 
-    term.output('Hello World');
-    expect(document.querySelector('output span:last-child').innerHTML).toEqual('Hello World');
+    term.output("Hello World");
+    expect(document.querySelector("output span:last-child").innerHTML).toEqual("Hello World");
   });
 
-  it('when prompt()', () => {
+  it("when prompt()", () => {
     const term = new WebTerminal();
 
-    term.prompt('Your name');
-    expect(document.querySelector('.command .prompt').innerHTML).toEqual('Your name:');
+    term.prompt("Your name");
+    expect(document.querySelector(".command .prompt").innerHTML).toEqual("Your name:");
   });
 
-  it('when setPrompt()', () => {
+  it("when setPrompt()", () => {
     const term = new WebTerminal();
 
-    term.setPrompt('Hello World ');
-    expect(document.querySelector('.command .prompt').innerHTML).toEqual('Hello World &gt;');
+    term.setPrompt("Hello World ");
+    expect(document.querySelector(".command .prompt").innerHTML).toEqual("Hello World &gt;");
   });
 
-  it('when set another container to instance', () => {
+  it("when set another container to instance", () => {
     document.body.innerHTML = '<div id="web-terminal-jest"></div>';
-    const term = new WebTerminal({ container: 'web-terminal-jest' });
+    const term = new WebTerminal({ container: "web-terminal-jest" });
 
     expect(term).toBeDefined();
   });
 
-  it('when customized instance', () => {
+  it("when customized instance", () => {
     const term = new WebTerminal({
-      welcome: 'Welcome JEST',
-      prompt: 'Testing with JEST ',
-      separator: '$',
+      welcome: "Welcome JEST",
+      prompt: "Testing with JEST ",
+      separator: "$",
     });
 
     expect(term).toBeDefined();
-    expect(document.querySelector('output span').innerHTML).toEqual('Welcome JEST');
-    expect(document.querySelector('.command .prompt').innerHTML).toEqual('Testing with JEST $');
+    expect(document.querySelector("output span").innerHTML).toEqual("Welcome JEST");
+    expect(document.querySelector(".command .prompt").innerHTML).toEqual("Testing with JEST $");
   });
 
-  it('when add new commands', () => {
+  it("when add new commands", () => {
     const mockCallback = jest.fn();
     const commands = { custom: mockCallback };
     const term = new WebTerminal({ commands });
 
     // Set the input value and simulate keydown
-    term.DOM.input.value = 'custom';
+    term.DOM.input.value = "custom";
     term.onKeyDown({ keyCode: 13 });
     expect(mockCallback.mock.calls.length).toBe(1);
   });
 
   // Color functionality tests
-  describe('Color functionality', () => {
-    it('should support disabling colors', () => {
+  describe("Color functionality", () => {
+    it("should support disabling colors", () => {
       const term = new WebTerminal({ colors: false });
       expect(term.contextualColors).toBe(false);
     });
 
-    it('should support enabling colors by default', () => {
+    it("should support enabling colors by default", () => {
       const term = new WebTerminal();
       expect(term.contextualColors).toBe(true);
     });
 
-    it('should support color themes', () => {
-      const term = new WebTerminal({ colorTheme: 'dark' });
-      expect(term.colorTheme).toBe('dark');
+    it("should support color themes", () => {
+      const term = new WebTerminal({ colorTheme: "dark" });
+      expect(term.colorTheme).toBe("dark");
     });
 
-    it('should default to light theme', () => {
+    it("should default to light theme", () => {
       const term = new WebTerminal();
-      expect(term.colorTheme).toBe('light');
+      expect(term.colorTheme).toBe("light");
     });
 
-    it('should output colored text when colors enabled', () => {
+    it("should output colored text when colors enabled", () => {
       const term = new WebTerminal({ colors: true });
       // Just test that the method completes without error
-      expect(() => term.output('Number: 42')).not.toThrow();
-      
+      expect(() => term.output("Number: 42")).not.toThrow();
+
       // Test that the contextual colorization is enabled
       expect(term.contextualColors).toBe(true);
     });
 
-    it('should support message types', () => {
+    it("should support message types", () => {
       const term = new WebTerminal({ colors: true });
       // Test that message type methods exist and don't throw
-      expect(() => term.error('Test error')).not.toThrow();
-      expect(() => term.success('Test success')).not.toThrow();
-      expect(() => term.warning('Test warning')).not.toThrow();
-      expect(() => term.info('Test info')).not.toThrow();
+      expect(() => term.error("Test error")).not.toThrow();
+      expect(() => term.success("Test success")).not.toThrow();
+      expect(() => term.warning("Test warning")).not.toThrow();
+      expect(() => term.info("Test info")).not.toThrow();
     });
 
-    it('should toggle colors at runtime', () => {
+    it("should toggle colors at runtime", () => {
       const term = new WebTerminal({ colors: true });
       expect(term.contextualColors).toBe(true);
       term.setContextualColors(false);
       expect(term.contextualColors).toBe(false);
     });
 
-    it('should apply theme CSS classes', () => {
-      const term = new WebTerminal({ colorTheme: 'light' });
-      expect(term.DOM.el.classList.contains('theme-light')).toBe(true);
+    it("should apply theme CSS classes", () => {
+      const term = new WebTerminal({ colorTheme: "light" });
+      expect(term.DOM.el.classList.contains("theme-light")).toBe(true);
     });
 
-    it('should switch theme CSS classes', () => {
-      const term = new WebTerminal({ colorTheme: 'light' });
-      expect(term.DOM.el.classList.contains('theme-light')).toBe(true);
-      
-      term.setColorTheme('dark');
-      expect(term.DOM.el.classList.contains('theme-dark')).toBe(true);
-      expect(term.DOM.el.classList.contains('theme-light')).toBe(false);
+    it("should switch theme CSS classes", () => {
+      const term = new WebTerminal({ colorTheme: "light" });
+      expect(term.DOM.el.classList.contains("theme-light")).toBe(true);
+
+      term.setColorTheme("dark");
+      expect(term.DOM.el.classList.contains("theme-dark")).toBe(true);
+      expect(term.DOM.el.classList.contains("theme-light")).toBe(false);
     });
 
-    it('should expose color utility functions', () => {
+    it("should expose color utility functions", () => {
       const term = new WebTerminal();
-      expect(typeof term.colors).toBe('object');
-      expect(typeof term.colors.red).toBe('function');
-      expect(typeof term.colors.bold).toBe('function');
-      expect(typeof term.colorize).toBe('function');
+      expect(typeof term.colors).toBe("object");
+      expect(typeof term.colors.red).toBe("function");
+      expect(typeof term.colors.bold).toBe("function");
+      expect(typeof term.colorize).toBe("function");
     });
 
-    it('should have working color utility functions', () => {
+    it("should have working color utility functions", () => {
       const term = new WebTerminal();
-      const redText = term.colors.red('test');
-      expect(redText).toContain('\x1b[31m');
-      expect(redText).toContain('test');
-      expect(redText).toContain('\x1b[0m');
-      
-      const colorizedText = term.colorize('test', '#ff0000');
-      expect(colorizedText).toContain('color: #ff0000');
-      expect(colorizedText).toContain('test');
+      const redText = term.colors.red("test");
+      expect(redText).toContain("\x1b[31m");
+      expect(redText).toContain("test");
+      expect(redText).toContain("\x1b[0m");
+
+      const colorizedText = term.colorize("test", "#ff0000");
+      expect(colorizedText).toContain("color: #ff0000");
+      expect(colorizedText).toContain("test");
+    });
+
+    it("should change theme and update existing text colors", () => {
+      const term = new WebTerminal({ colorTheme: "dark", contextualColors: true });
+
+      // Add some output with colors
+      term.output("Number: 42");
+      term.output("URL: https://example.com");
+
+      // Get the spans with inline color styles
+      const outputSpans = term.DOM.output.querySelectorAll('span[style*="color"]');
+      expect(outputSpans.length).toBeGreaterThan(0);
+
+      // Store initial color from first span
+      const firstSpan = outputSpans[0];
+      const initialStyle = firstSpan.getAttribute("style");
+
+      // Change theme
+      term.setColorTheme("light");
+
+      // Verify theme changed
+      expect(term.colorTheme).toBe("light");
+      expect(term.DOM.el.classList.contains("theme-light")).toBe(true);
+      expect(term.DOM.el.classList.contains("theme-dark")).toBe(false);
+
+      // Verify the color in the style attribute changed
+      const updatedStyle = firstSpan.getAttribute("style");
+      expect(updatedStyle).not.toBe(initialStyle);
     });
   });
 });
